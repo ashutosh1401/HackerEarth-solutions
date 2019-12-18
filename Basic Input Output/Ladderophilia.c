df95ba45b6ecd1e27f080a78c67109b47bf59923
ashutosh1401 <56783815+ashutosh1401@users.noreply.github.com>
2019-12-18 22:56:42 +0530

    Create Ladderophilia.c

---
+++ Basic Input Output/Ladderophilia.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main(){
+	int n,i;
+	scanf("%d",&n);
+	printf("*   *\n");
+	for(i=1;i<=n;i++)
+	{
+	  printf("*   *\n");
+	  printf("*****\n");
+	  printf("*   *\n");
+	  
+	  } printf("*   *\n");
+	  return 0;
+}
