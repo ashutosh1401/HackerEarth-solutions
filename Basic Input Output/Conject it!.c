c9d86032c8c69bc3dbd0c376b00d3b996ffa5add
ashutosh1401 <56783815+ashutosh1401@users.noreply.github.com>
2019-12-18 23:00:54 +0530

    Create Conject it!.c

---
+++ Basic Input Output/Conject it!.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main(){
+	int t,n,i;
+	scanf("%d\n",&t);
+	for(i=0;i<t;i++)
+	{ scanf("%d\n",&n);
+          printf("YES\n");
+          //As all numbers would return YES 
+          }
+                 
+}
+ 
