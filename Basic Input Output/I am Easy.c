70f17643b676c381cfda41227c5e4b7090501131
ashutosh1401 <56783815+ashutosh1401@users.noreply.github.com>
2019-12-18 22:53:25 +0530

    Create I am Easy.c

---
+++ Basic Input Output/I am Easy.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main(){
+	int num,i,t;
+	scanf("%d", &num);              			
+    for(i=1;i<=10;i++) 
+    { t=num*i;
+      printf("%d\n",t);
+    }
+    return 0;// 
+}
+ 
