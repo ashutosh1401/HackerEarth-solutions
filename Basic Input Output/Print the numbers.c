616fc4a02aab1ee6a064093e259d61e857283935
ashutosh1401 <56783815+ashutosh1401@users.noreply.github.com>
2019-12-19 09:30:10 +0530

    Create Print the numbers.c

---
+++ Basic Input Output/Print the numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main(){
+	int num,t,i;
+	scanf("%d", &num);              		
+    for(i=1; i<=num;i++)
+    { scanf("%d",&t);
+      printf("%d ",t);
+      }
+      return 0;
+}
