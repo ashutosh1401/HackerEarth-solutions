9efc80d617346ee7350ee6855bc4a15f74f0f1bb
ashutosh1401 <56783815+ashutosh1401@users.noreply.github.com>
2019-12-19 09:31:47 +0530

    Create Print the numbers.cpp

---
+++ Basic Input Output/Print the numbers.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() { 
+    int i,t,n; 
+    cin >>n; 
+    for(i=0;i<n;i++)
+    { cin >>t; 
+       cout << t<<" ";}// Writing output to STDOUT
+}
