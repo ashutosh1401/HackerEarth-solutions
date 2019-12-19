d2189e2359ed2b85de24dc431ecf37de71e825b3
ashutosh1401 <56783815+ashutosh1401@users.noreply.github.com>
2019-12-19 09:45:49 +0530

    Create Doctor's secret.cpp

---
+++ Basic Input Output/Doctor's secret.cpp
@@ -0,0 +1,16 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	int n,t;
+	cin>>n;
+	cin>>t;
+    if(n<=23&&500<=t<=100)
+      cout<<"Take Medicine";
+    else
+      cout<<"Don't take Medicine";
+      return 0;
+									
+		
+}
